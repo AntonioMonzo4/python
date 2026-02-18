b1fc22f1a6a029cd7adbd0a1c9d937327237f5a3
Antonio Monzo <AMONANSU@GMAIL.COM>
2026-02-18 17:53:58 +0100

    Variales 1

---
+++ 01_basic/04_variables.py
@@ -0,0 +1,3 @@
+#Las variable son espacios de memoria que se utilizan para almacenar datos. En Python, 
+#no es necesario declarar el tipo de dato de una variable, 
+#ya que el intérprete lo infiere automáticamente.   
