fb5667e4a6be45d01dd13f0c22f77578f7e3aba3
Antonio Monzo <AMONANSU@GMAIL.COM>
2026-02-18 16:04:27 +0100

    Print y comentarios

--- 01_basic/01_print.py
+++ 01_basic/01_print.py
@@ -1,1 +1,19 @@
-print("Hola Mundo!!") 
+print("Hola Mundo!!") # Imprime el mensaje "Hola Mundo!!" en la consola
+
+#También puedes usar comillas simples para imprimir el mismo mensaje
+print('Hola Mundo!') # Imprime el mensaje "Hola Mundo!!" en la consola
+
+#Se pueden imprimir varios valores separados por comas
+print("Hola", "Mundo", "!!") # Imprime los valores "Hola",
+# "Mundo" y "!!" separados por espacios en la consola
+
+#También puedes usar el operador de concatenación para unir cadenas
+print("Hola" + " " + "Mundo" + "!!") # Imprime el mensaje "Hola Mundo!!"
+#Se puede usar un separador personalizado al imprimir varios valores
+print("Hola", "Mundo", "!!", sep="-") # Imprime los valores "Hola", "Mundo" y "!!" separados por guiones en la consola
+
+#Puedes usar el formato de cadena para imprimir variables
+nombre = "Antonio"
+edad = 25
+print(f"Mi nombre es {nombre} y tengo {edad} años") # Imprime el mensaje "Mi nombre es Antonio y tengo 25 años" utilizando 
+#el formato de cadena con variables
