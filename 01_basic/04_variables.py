8fe012b11f09a09966ae401940b9fad77ede2ef2
Antonio Monzo <AMONANSU@GMAIL.COM>
2026-02-18 18:01:48 +0100

    Variales 1

--- 01_basic/04_variables.py
+++ 01_basic/04_variables.py
@@ -1,3 +1,9 @@
 #Las variable son espacios de memoria que se utilizan para almacenar datos. En Python, 
 #no es necesario declarar el tipo de dato de una variable, 
 #ya que el intérprete lo infiere automáticamente.   
+
+#Para crear una variable, simplemente asigna un valor a un nombre utilizando el operador de asignación (=)
+mi_variable = 10
+print(mi_variable) # Imprime el valor 10 en la consola
+#Puedes asignar cualquier tipo de dato a una variable
+mi_variable = "Hola Mundo"
