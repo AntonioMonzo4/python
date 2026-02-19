9e6d1b6ca57c1d4662223dac9d460f89ae927907
Antonio Monzo <AMONANSU@GMAIL.COM>
2026-02-19 14:44:06 +0100

    Operadores lógicos

--- 02_flow_control/01_if.py
+++ 02_flow_control/01_if.py
@@ -21,3 +21,22 @@ elif nota >= 60:
 else:
     print("Insuficiente")
     
+
+#OPCIONES DE OPERADORES LÓGICOS
+#AND (y): Devuelve True si ambas condiciones son verdaderas.
+#OR (o): Devuelve True si al menos una de las condiciones es verdadera.
+#NOT (no): Devuelve True si la condición es falsa, y False si la condición es verdadera.
+edad = 25
+if edad >= 18 and edad < 65:
+    print("Eres un adulto.")
+if edad < 18 or edad >= 65:
+    print("No eres un adulto.")
+if not (edad < 18):
+    print("Eres mayor de edad.")
+
+#OPERADOR TERNARIO
+#El operador ternario es una forma concisa de escribir una expresión condicional en una sola línea. La sintaxis general es:
+#valor_si_verdadero if condición else valor_si_falso
+edad = 20
+mensaje = "Eres mayor de edad." if edad >= 18 else "Eres menor de edad."
+print(mensaje)
