ace725cb297313b8ce893689b4551e03e8061cd3
Antonio Monzo <AMONANSU@GMAIL.COM>
2026-02-23 12:15:41 +0100

    Listas 2

--- 02_flow_control/03_list.py
+++ 02_flow_control/03_list.py
@@ -3,12 +3,15 @@
 frutas = ["manzana", "banana", "naranja"]
 numeros = [1, 2, 3, 4, 5]
 mixta = ["texto", 42, 3.14, True]
+
 #Acceso a elementos de una lista
 #Puedes acceder a los elementos de una lista utilizando índices. El índice comienza en 0 para el primer elemento, 1 para el segundo, y así sucesivamente.
 print(frutas[0])  # Imprime "manzana"
 print(numeros[2])  # Imprime 3
 print(mixta[1])    # Imprime 42
+
 #Modificación de elementos de una lista
 #Las listas son mutables, lo que significa que puedes cambiar sus elementos después de haberlas creado.
 frutas[1] = "pera"
 print(frutas)  # Imprime ["manzana", "pera", "naranja"]
+
