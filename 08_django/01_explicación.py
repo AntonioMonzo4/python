98eeb3000a3a6acc729ef03b456f7fad80923415
Antonio Monzo <AMONANSU@GMAIL.COM>
2026-03-12 09:41:09 +0100

    feat(DJANGO): Explicación

---
+++ 08_django/01_explicación.py
@@ -0,0 +1,4 @@
+#Django es un framework de desarrollo web de alto nivel que sigue el patrón de diseño Modelo-Vista-Controlador (MVC). 
+# Fue creado para facilitar el desarrollo rápido y limpio de aplicaciones web. 
+# Django proporciona una estructura sólida y herramientas integradas para manejar tareas comunes en el desarrollo web, como la gestión de bases de datos, 
+# la autenticación de usuarios, la administración de contenido, entre otras.
