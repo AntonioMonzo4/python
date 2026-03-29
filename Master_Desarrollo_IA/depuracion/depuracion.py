dc29441b10ca72682faa01739752d30d998a2699
AntonioMonzo <amonansu@gmail.com>
2026-03-29 20:13:23 +0200

    Test Unitarios en unittest

--- Master_Desarrollo_IA/depuracion/depuracion.py
+++ Master_Desarrollo_IA/depuracion/depuracion.py
@@ -29,3 +29,24 @@ print(f"Precio final con descuento: {precio_con_descuento}")
 # Esto se puede hacer utilizando herramientas de depuración integradas en 
 # los entornos de desarrollo (IDEs) 
 # o mediante la inserción de puntos de interrupción (breakpoints) en el código.
+
+
+import unittest #Importar el módulo unittest para crear pruebas unitarias
+#Ejemplo de función a depurar
+#Como funciona unittest: Es un marco de pruebas unitarias que permite crear y
+#  ejecutar pruebas para verificar el correcto funcionamiento de las funciones.
+# Para usar unittest, se define una clase que hereda de unittest.
+# TestCase y se crean métodos de prueba dentro de esa clase.
+
+def sumar(a, b):
+    return a + b
+#Prueba unitaria para la función sumar
+class TestSumar(unittest.TestCase):
+    def test_sumar(self):
+        self.assertEqual(sumar(2, 3), 5) #Prueba correcta
+        self.assertEqual(sumar(-1, 1), 0) #Prueba correcta
+        self.assertEqual(sumar(0, 0), 0) #Prueba correcta
+        self.assertEqual(sumar(2, -3), -1) #Prueba correcta
+if __name__ == '__main__':
+    unittest.main() #Ejecutar las pruebas unitarias
+    
