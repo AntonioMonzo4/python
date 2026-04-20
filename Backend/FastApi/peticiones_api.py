3f296a7771c021a9fe72c896a20f49b8c66268a2
Antonio Monzo <AMONANSU@GMAIL.COM>
2026-04-20 19:33:48 +0200

    Operaciones BBDD

--- Backend/FastApi/peticiones_api.py
+++ Backend/FastApi/peticiones_api.py
@@ -3,3 +3,8 @@
 #PUT: para actualizar datos en el servidor / reemplaza datos en el servidor 
 #DELETE: eliminar datos en el servidor 
 
+#OPTIONS: para obtener información sobre las opciones de comunicación con el servidor
+#HEAD: para obtener los encabezados de una respuesta sin el cuerpo
+#PATCH: para actualizar parcialmente un recurso en el servidor
+#TRACE: para realizar una prueba de bucle de retorno en el servidor
+#CONNECT: para establecer un túnel de comunicación con el servidor
