bd4e2f782df1a89e5ccef633c4cf346d7dbae7ed
Antonio Monzo <AMONANSU@GMAIL.COM>
2026-04-20 19:27:46 +0200

    Operaciones Get

--- Backend/FastApi/users.py
+++ Backend/FastApi/users.py
@@ -8,14 +8,14 @@ app = FastAPI()
 
 #Entidad User
 class User(BaseModel):
+    id: int
     name: str
     age: int
     username: str 
 
-users_list = [User(name="Antonio",age=26,username="antonio_dev"),
-            User(name="Tesla",age=100,username="tesla_dev"),
-            User(name="Einstein",age=150,username="einstein_dev")
-         ]#Para que no exista duda de como se crean los objetos 
+users_list = [User(id :42, name="Antonio",age=26,username="antonio_dev"),
+            User(id:27, name="Tesla",age=100,username="tesla_dev"),
+            User(id:48,name="Einstein",age=150,username="einstein_dev")]#Para que no exista duda de como se crean los objetos 
 
 
 
@@ -25,7 +25,27 @@ async def users_json():
             {"name":"Tesla","age":100,"username":"tesla_dev"},
             {"name":"Einstein","age":150,"username":"einstein_dev"}
             ]
+#Este se hace mediante el path 
+@app.get("/user/{id}")
+async def users(id: int ):
+    users = filter(lambda user: user.id == id, users_list)
+    try: 
+        return list(users)[0]
+    except: 
+        return {"error": "User not found"}
 
-@app.get("/users")
-async def users():
+#Este se hace mediante la query
+@app.get("/userquery/")
+async def users(id: int ):
+   return search_user(id)
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try: 
+        return list(users)[0]
+    except: 
+        return {"error": "User not found"}
+
+@app.get("/users/")
+async def user():
     return users_list
