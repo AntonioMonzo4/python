32012aaa131a9a6f45c2e3bc4358a48d0474b706
Antonio Monzo <AMONANSU@GMAIL.COM>
2026-04-21 15:02:37 +0200

    imágenes y estáticos

--- Backend/FastApi/main.py
+++ Backend/FastApi/main.py
@@ -1,12 +1,18 @@
 #Previamente se ha instalado FastApi con el comando pip install "fastapi[all]"
 from fastapi import FastAPI
 from routers import products
+from fastapi.staticfiles import StaticFiles
+
+
 
 app = FastAPI()
 
 
 # Routers
 app.include_router(products.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")#Para montar archivos estáticos como imágenes,
+#CSS o JavaScript en la ruta /static. El directorio "static" es donde se encuentran estos archivos en el proyecto. 
+# El nombre "static" es un alias para esta ruta, que se puede usar para referenciar los archivos estáticos en otras partes de la aplicación.
 
 
 @app.get("/")# @app(decorador) sirve para un objeto web en este caso define la ruta del get  en este caso el localhost 
