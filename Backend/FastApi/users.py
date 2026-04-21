fc7464105d951825105674c783f065b1f3fa7420
Antonio Monzo <AMONANSU@GMAIL.COM>
2026-04-21 12:54:04 +0200

    Estoy haciendo pruebas con el teclado más métodos

--- Backend/FastApi/users.py
+++ Backend/FastApi/users.py
@@ -25,6 +25,7 @@ async def users_json():
             {"name":"Tesla","age":100,"username":"tesla_dev"},
             {"name":"Einstein","age":150,"username":"einstein_dev"}
             ]
+
 #Este se hace mediante el path 
 @app.get("/user/{id}")
 async def users(id: int ):
@@ -49,3 +50,45 @@ def search_user(id: int):
 @app.get("/users/")
 async def user():
     return users_list
+
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id) )== User:
+        return {"error": "User already exists"}
+    else:
+        users_list.append(user)
+
+#El put se hace para actualizar un usuario, el delete se hace para eliminar un usuario
+#Patch se hace para actualizar parcialmente un usuario, es decir, solo algunos campos del usuario, 
+# mientras que el put se hace para actualizar completamente un usuario, es decir, todos los campos del usuario.
+@app.put("/user/")
+async def user(user:User):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+            break
+    if not found:
+        return {"error": "User not found"}
+    return {"message": "User updated"}
+
+#Si envias datos en el body pero no se definen  en la función FastApi no lo va a tomar en cuenta, 
+# por eso es importante definir los parámetros que se van a recibir en la función, 
+# para que FastApi pueda validar la información y evitar errores.
+
+@app.delete("/user/{id}")
+async def user(id: int):
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+            break
+    if not found:
+        return {"error": "User not found"}
+    return {"message": "User deleted"}
+    
